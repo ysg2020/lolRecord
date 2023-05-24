f84cf4b91a28c60b5200affea25af1f441789d9a
YSG <dbtjrrbs12@naver.com>
2023-05-24 23:23:18 +0900

    1. 인터셉터 처리

--- src/main/java/hello/lolRecord/common/WebConfigure.java
+++ src/main/java/hello/lolRecord/common/WebConfigure.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,6 +18,15 @@ public class WebConfigure implements WebMvcConfigurer , WebMvcRegistrations {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
     private final ModelAndViewArgumentResolver config;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/LOLRecord/lolUser","/LOLRecord/lolUser/loginForm","/LOLRecord/lolUser/joinForm"
+                                        ,"/LOLRecord/lolUser/login","/LOLRecord/lolUser/logout");
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
